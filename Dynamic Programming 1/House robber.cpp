f0e2dc03c537689306e2287d7ecf9543d743c482
Ziaurrehman Malik <112377951+Ziaurrehman90@users.noreply.github.com>
2024-05-23 16:55:46 +0530

    Add files via upload

---
+++ Dynamic Programming 1/House robber.cpp
@@ -0,0 +1,28 @@
+class Solution
+{
+public:
+    int f(int index, vector<int> &nums, vector<int> &dp)
+    {
+        if (index == 0)
+        {
+            return nums[index];
+        }
+        if (index < 0)
+        {
+            return 0;
+        }
+        if (dp[index] != -1)
+        {
+            return dp[index];
+        }
+        int take = nums[index] + f(index - 2, nums, dp);
+        int not_take = 0 + f(index - 1, nums, dp);
+        return dp[index] = max(take, not_take);
+    }
+    int rob(vector<int> &nums)
+    {
+        int n = nums.size();
+        vector<int> dp(n + 1, -1);
+        return f(n - 1, nums, dp);
+    }
+};
